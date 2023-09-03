dd09d11e89b671cd4f8c43ff58232413455fbf3b
Arthur Gareginyan <arthurgareginyan@gmail.com>
2023-09-04 01:09:56 +0300

    Add: Utility function to toggle display

--- renderer.js
+++ renderer.js
@@ -1,20 +1,30 @@
 const { ipcRenderer } = require('electron')
 const { marked } = require('marked')
 
+// Cache frequently accessed DOM elements
+const helperWindow = document.getElementById('helper-window')
+
+// Utility function to toggle display
+function toggleDisplay(element, condition) {
+    if (element) {
+        element.style.display = condition ? 'block' : 'none'
+    }
+}
+
 ipcRenderer.on('ready', (event) => {
     ipcRenderer.send('open-file')
-    document.getElementById('main-title').style.display = 'block';
+    toggleDisplay(helperWindow, true)
 })
 
 ipcRenderer.on('file-content', (event, fileContent) => {
     const markdownContainer = document.getElementById('markdown-container')
     const renderedMarkdown = marked(fileContent)
     markdownContainer.innerHTML = renderedMarkdown
-    document.getElementById('main-title').style.display = 'none';
+    toggleDisplay(helperWindow, false)
 })
 
 ipcRenderer.on('clear-content', (event) => {
     const markdownContainer = document.getElementById('markdown-container')
     markdownContainer.innerHTML = ''
-    document.getElementById('main-title').style.display = 'block';
+    toggleDisplay(helperWindow, true)
 })
